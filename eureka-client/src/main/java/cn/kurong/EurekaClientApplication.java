1383021a49e098c2ef86f6157992dc1367e7d83f
Administrator <1181209156@qq.com>
2020-01-05 22:32:37 +0800

    client

---
+++ eureka-client/src/main/java/cn/kurong/EurekaClientApplication.java
@@ -0,0 +1,18 @@
+package cn.kurong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @ClassName EurekaClientApplication
+ * @Description EurekaClient
+ * @Author Administrator
+ * @Date 2020/1/5 20:39
+ * @Version 1.0
+ */
+@SpringBootApplication
+public class EurekaClientApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaClientApplication.class, args);
+    }
+}
