acaf66382e6266a96b8c137b2d60d5c48ab4ef06
Administrator <1181209156@qq.com>
2020-01-05 19:12:41 +0800

    Initial commit

---
+++ eureka-server/src/main/java/cn/kurong/EurekaServerApplication.java
@@ -0,0 +1,20 @@
+package cn.kurong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @ClassName EurekaServerApplication
+ * @Description TODO
+ * @Author Administrator
+ * @Date 2020/1/5 18:42
+ * @Version 1.0
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
